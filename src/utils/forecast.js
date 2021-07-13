239be0c6cc8c36eda520130f6b1c03cd8ad2e983
scotthetrick <hetrick.scott@gmail.com>
2021-07-12 23:08:08 -0400

    Add humidity to forecast data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast = (latitude, longitude, callback) => {
             callback('Unable to find location')
         }
         else {
-            const data = body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' +  body.current.feelslike + ' degrees out.'
+            const data = body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' +  body.current.feelslike + ' degrees out. The humidity is ' + body.current.humidity + '%.'
             callback(undefined, data)
         }
     })
